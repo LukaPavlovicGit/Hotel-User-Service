40b498aedbbda36ef543801c8a7096e35699faaa
lukapavlovic <lpavlovic11521rn@raf.rs>
2022-09-04 01:55:48 +0200

    initializing user service project

---
+++ src/test/java/com/raf/example/HotelUserService/HotelUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raf.example.HotelUserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
